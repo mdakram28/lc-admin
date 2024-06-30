eb105b76a8b3984a1d353c1eef2f7872ffe721ca
mdakram28 <mdakram28@gmail.com>
2024-06-29 21:03:17 -0400

    sync

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 
-export LC_CONTEST="weekly-contest-400"
+export LC_CONTEST="weekly-contest-399"
 export LC_QUESNUM=4
 export REPORT="${LC_CONTEST}_Q${LC_QUESNUM}"
 
-set -e
+# set -e
 
 python.exe scripts/pre_plag.py ${LC_CONTEST} ${LC_QUESNUM}
 
@@ -11,7 +11,5 @@ docker run --init --network host \
     -v "$(pwd)/public/contests/$REPORT:/dolos" ghcr.io/dodona-edu/dolos-cli -f csv -l cpp info.csv
 
 mv ./public/contests/$REPORT/dolos-report-* ./public/contests/$REPORT/dolos-report
-
-sleep 3
 
 python.exe scripts/post_plag.py ${LC_CONTEST} ${LC_QUESNUM}
